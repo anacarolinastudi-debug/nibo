b4a9e8d3dd51aceadd8b7a5e6bc56e8dbac5cdb5
anacarolinastudi-debug <anacarolina.studi@gmail.com>
2026-06-18 11:29:58 -0400

    Delete src directory

--- src/lib/prisma.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-
-// Instância única do Prisma reaproveitada em toda a aplicação.
-// Evita abrir uma conexão nova com o banco em cada requisição.
-const prisma = new PrismaClient();
-
-module.exports = prisma;
